bfc3fdc4e1224a57a5839b702702199233009695
Pakin Suwannawat <painter.suwannawat@gmail.com>
2018-10-10 21:11:31 +0700

    add styled-components package

--- rollup.config.js
+++ rollup.config.js
@@ -13,25 +13,27 @@ export default {
     {
       file: pkg.main,
       format: 'cjs',
-      sourcemap: true
+      sourcemap: true,
     },
     {
       file: pkg.module,
       format: 'es',
-      sourcemap: true
-    }
+      sourcemap: true,
+    },
   ],
   plugins: [
     external(),
     postcss({
-      modules: true
+      modules: true,
     }),
     url(),
     babel({
       exclude: 'node_modules/**',
-      plugins: [ 'external-helpers' ]
+      plugins: ['external-helpers'],
     }),
     resolve(),
-    commonjs()
-  ]
+    commonjs(),
+  ],
+  external: ['styled-components'],
+  global: { 'styled-components': 'styled' },
 }
