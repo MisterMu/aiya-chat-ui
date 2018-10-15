e0a531ed4c0f5e0793079a2e90105fe5a59be369
Pakin Suwannawat <painter.suwannawat@gmail.com>
2018-10-15 16:16:14 +0700

    create color constants

---
+++ src/constants/color.js
@@ -0,0 +1,11 @@
+const FACEBOOK = {
+  textColor: '#45494F',
+  bubbleColor: '#F1F0F0',
+}
+
+const LINE = {
+  textColor: '#314F22',
+  bubbleColor: '#BEF18C',
+}
+
+export { FACEBOOK, LINE }
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,5 @@
 import channelTypes from './channelTypes'
 import messageTypes from './messageTypes'
+import * as color from './color'
 
-export { channelTypes, messageTypes }
+export { channelTypes, messageTypes, color }
