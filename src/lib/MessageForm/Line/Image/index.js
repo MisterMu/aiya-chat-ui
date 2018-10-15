d17830b2801345b0ad9416dcd9fcaab6d06856ba
Pakin Suwannawat <painter.suwannawat@gmail.com>
2018-10-15 15:25:57 +0700

    add preview image field

--- src/lib/MessageForm/Line/Image/index.js
+++ src/lib/MessageForm/Line/Image/index.js
@@ -4,22 +4,28 @@ import BaseMessageForm from '../../BaseMessageForm'
 import InputField from '../../InputField'
 
 class ImageMessageForm extends BaseMessageForm {
-  inputChange = value => {
+  inputChange = (key, value) => {
     const { message } = this.state
     let tmp = { ...message }
-    tmp.originalContentUrl = value
+    tmp[key] = value
     this.setState({ message: tmp })
   }
 
   renderForm = () => {
-    const { originalContentUrl } = this.state.message
+    const { originalContentUrl, previewImageUrl } = this.state.message
     return (
       <form onSubmit={this.onSubmit}>
-        <InputField label="Url">
+        <InputField label="Preview Image">
           <Input
             value={originalContentUrl}
-            onChange={e => this.inputChange(e.target.value)}
+            onChange={e => this.inputChange('originalContentUrl', e.target.value)}
             autoFocus
+          />
+        </InputField>
+        <InputField label="Full Image">
+          <Input
+            value={previewImageUrl}
+            onChange={e => this.inputChange('previewImageUrl', e.target.value)}
           />
         </InputField>
       </form>
