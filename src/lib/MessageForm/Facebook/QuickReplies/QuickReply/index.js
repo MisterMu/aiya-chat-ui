ee89faf42a085f897ad406932b19b883628f1d2c
Pakin Suwannawat <painter.suwannawat@gmail.com>
2018-10-16 20:37:14 +0700

    complete Facebook QuickReplies MessageForm

---
+++ src/lib/MessageForm/Facebook/QuickReplies/QuickReply/index.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Select, Input } from 'antd'
+
+import InputField from '../../../InputField'
+import { Types } from '../../../../MessageObject/Facebook/quickReply'
+
+const QuickReplyForm = props => {
+  const { data, dataChange } = props
+  if (!data) {
+    return null
+  }
+  return (
+    <div style={{ marginBottom: 8 }}>
+      <InputField label="Type">
+        <Select
+          defaultValue={data.content_type}
+          onChange={value => dataChange({ content_type: value })}
+          style={{ width: 180 }}
+        >
+          {Object.keys(Types).map(key => (
+            <Select.Option value={Types[key]} key={key}>
+              <span>{Types[key]}</span>
+            </Select.Option>
+          ))}
+        </Select>
+      </InputField>
+      {data.content_type === Types.TEXT && (
+        <React.Fragment>
+          <InputField label="Title">
+            <Input value={data.title} onChange={e => dataChange({ title: e.target.value })} />
+          </InputField>
+          <InputField label="Payload">
+            <Input value={data.payload} onChange={e => dataChange({ payload: e.target.value })} />
+          </InputField>
+          <InputField label="Image Url">
+            <Input
+              value={data.image_url}
+              onChange={e => dataChange({ image_url: e.target.value })}
+            />
+          </InputField>
+        </React.Fragment>
+      )}
+    </div>
+  )
+}
+
+QuickReplyForm.propTypes = {
+  data: PropTypes.object.isRequired,
+  dataChange: PropTypes.func.isRequired,
+}
+
+export default QuickReplyForm
---
+++ src/lib/MessageForm/Facebook/QuickReplies/index.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Divider } from 'antd'
+import QuickReplyForm from './QuickReply'
+import BaseMessageForm from '../../BaseMessageForm'
+import QuickReplyObject from '../../../MessageObject/Facebook/quickReply'
+
+class QuickRepliesForm extends BaseMessageForm {
+  inputChange = (item, index) => {
+    const { message } = this.state
+    let tmp = [...message.quick_replies]
+    const oldData = { ...tmp[index] }
+    if (item.content_type && item.content_type !== oldData.content_type) {
+      tmp[index] = QuickReplyObject[item.content_type]
+    } else {
+      tmp[index] = { ...oldData, ...item }
+    }
+    this.setState({ message: { ...message, quick_replies: tmp } })
+  }
+
+  renderForm = () => {
+    const { message } = this.state
+    return (
+      <form onSubmit={this.onSubmit} style={{ paddingLeft: 8, paddingRight: 8 }}>
+        {message &&
+          (message.quick_replies || []).map((quickReply, i) => (
+            <React.Fragment key={i}>
+              <Divider orientation="left">Quick Reply #{i + 1}</Divider>
+              <QuickReplyForm data={quickReply} dataChange={item => this.inputChange(item, i)} />
+            </React.Fragment>
+          ))}
+      </form>
+    )
+  }
+}
+
+export default QuickRepliesForm
