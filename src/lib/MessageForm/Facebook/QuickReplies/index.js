fb805a4d378d9d00183b447ad2a11d155295b5d2
Pakin Suwannawat <painter.suwannawat@gmail.com>
2018-10-16 21:54:11 +0700

    can add and delete QuickReply in QuickReplies Form

--- src/lib/MessageForm/Facebook/QuickReplies/index.js
+++ src/lib/MessageForm/Facebook/QuickReplies/index.js
@@ -1,13 +1,42 @@
 import React from 'react'
-import { Divider } from 'antd'
+import { Divider, Icon } from 'antd'
+import styled from 'styled-components'
 import QuickReplyForm from './QuickReply'
 import BaseMessageForm from '../../BaseMessageForm'
 import QuickReplyObject from '../../../MessageObject/Facebook/quickReply'
 
+const Flex = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  i:hover {
+    color: red;
+    cursor: pointer;
+  }
+`
+
 class QuickRepliesForm extends BaseMessageForm {
+  addQuickReply = () => {
+    const { message } = this.state
+    if (message && message.quick_replies) {
+      const tmp = [...message.quick_replies, QuickReplyObject.text]
+      this.setState({ message: { ...message, quick_replies: tmp } })
+    }
+  }
+
+  delQuickReply = index => {
+    const { message } = this.state
+    if (message && message.quick_replies) {
+      let tmp = [...message.quick_replies]
+      tmp.splice(index, 1)
+      this.setState({ message: { ...message, quick_replies: tmp } })
+    }
+  }
+
   inputChange = (item, index) => {
     const { message } = this.state
-    if (message.quick_replies) {
+    if (message && message.quick_replies) {
       let tmp = [...message.quick_replies]
       const oldData = { ...tmp[index] }
       if (item.content_type && item.content_type !== oldData.content_type) {
@@ -21,15 +50,32 @@ class QuickRepliesForm extends BaseMessageForm {
 
   renderForm = () => {
     const { message } = this.state
+    if (!message) {
+      return null
+    }
+
     return (
       <form onSubmit={this.onSubmit} style={{ paddingLeft: 8, paddingRight: 8 }}>
-        {message &&
-          (message.quick_replies || []).map((quickReply, i) => (
-            <React.Fragment key={i}>
+        {(message.quick_replies || []).map((quickReply, i) => (
+          <React.Fragment key={i}>
+            <Flex>
               <Divider orientation="left">Quick Reply #{i + 1}</Divider>
-              <QuickReplyForm data={quickReply} dataChange={item => this.inputChange(item, i)} />
-            </React.Fragment>
-          ))}
+              <Icon
+                style={{ marginLeft: 8 }}
+                type="close-circle"
+                onClick={() => this.delQuickReply(i)}
+              />
+            </Flex>
+            <QuickReplyForm data={quickReply} dataChange={item => this.inputChange(item, i)} />
+          </React.Fragment>
+        ))}
+        <Divider />
+        {message.quick_replies.length < 10 && (
+          <Flex style={{ cursor: 'pointer' }} onClick={this.addQuickReply}>
+            <Icon type="plus-circle" />
+            <span style={{ marginLeft: 8 }}>Add QuickReply</span>
+          </Flex>
+        )}
       </form>
     )
   }
--- src/lib/MessageForm/Line/QuickReplies/index.js
+++ src/lib/MessageForm/Line/QuickReplies/index.js
@@ -1,10 +1,39 @@
 import React from 'react'
-import { Divider } from 'antd'
+import { Divider, Icon } from 'antd'
+import styled from 'styled-components'
 import QuickReplyForm from './QuickReply'
 import BaseMessageForm from '../../BaseMessageForm'
 import QuickReplyObject from '../../../MessageObject/Line/quickReply'
 
+const Flex = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  i:hover {
+    color: red;
+    cursor: pointer;
+  }
+`
+
 class QuickRepliesForm extends BaseMessageForm {
+  addQuickReply = () => {
+    const { message } = this.state
+    if (message && message.quickReply) {
+      const tmp = [...message.quickReply.items, QuickReplyObject.message]
+      this.setState({ message: { ...message, quickReply: { items: tmp } } })
+    }
+  }
+
+  delQuickReply = index => {
+    const { message } = this.state
+    if (message && message.quickReply) {
+      let tmp = [...message.quickReply.items]
+      tmp.splice(index, 1)
+      this.setState({ message: { ...message, quickReply: { items: tmp } } })
+    }
+  }
+
   inputChange = (item, index) => {
     const { message } = this.state
     if (message && message.quickReply) {
@@ -34,16 +63,31 @@ class QuickRepliesForm extends BaseMessageForm {
 
   renderForm = () => {
     const { message } = this.state
+    if (!message || !message.quickReply) {
+      return null
+    }
     return (
       <form onSubmit={this.onSubmit} style={{ paddingLeft: 8, paddingRight: 8 }}>
-        {message &&
-          message.quickReply &&
-          (message.quickReply.items || []).map((quickReply, i) => (
-            <React.Fragment key={i}>
+        {(message.quickReply.items || []).map((quickReply, i) => (
+          <React.Fragment key={i}>
+            <Flex>
               <Divider orientation="left">Quick Reply #{i + 1}</Divider>
-              <QuickReplyForm data={quickReply} dataChange={item => this.inputChange(item, i)} />
-            </React.Fragment>
-          ))}
+              <Icon
+                style={{ marginLeft: 8 }}
+                type="close-circle"
+                onClick={() => this.delQuickReply(i)}
+              />
+            </Flex>
+            <QuickReplyForm data={quickReply} dataChange={item => this.inputChange(item, i)} />
+          </React.Fragment>
+        ))}
+        <Divider />
+        {message.quickReply.items.length < 10 && (
+          <Flex style={{ cursor: 'pointer' }} onClick={this.addQuickReply}>
+            <Icon type="plus-circle" />
+            <span style={{ marginLeft: 8 }}>Add QuickReply</span>
+          </Flex>
+        )}
       </form>
     )
   }
