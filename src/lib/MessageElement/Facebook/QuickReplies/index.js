5473f175fbae5d19d2e107d81e4cefb47bd98dd2
Pakin Suwannawat <painter.suwannawat@gmail.com>
2018-10-16 17:43:18 +0700

    change Element to state-less function

--- src/lib/MessageElement/Facebook/QuickReplies/index.js
+++ src/lib/MessageElement/Facebook/QuickReplies/index.js
@@ -3,26 +3,19 @@ import PropTypes from 'prop-types'
 import QuickReplyElement from './QuickReply'
 import { Flex } from '../../styled'
 
-class QuickRepliesElement extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
-  }
-
-  render() {
-    const { quickReplies } = this.props
-    return (
-      <Flex style={{ justifyContent: 'center' }}>
-        {quickReplies.map((quickReply, key) => {
-          return (
-            <React.Fragment key={key}>
-              <QuickReplyElement data={quickReply} />
-            </React.Fragment>
-          )
-        })}
-      </Flex>
-    )
-  }
+const QuickRepliesElement = props => {
+  const { quickReplies } = props
+  return (
+    <Flex style={{ justifyContent: 'center' }}>
+      {quickReplies.map((quickReply, key) => {
+        return (
+          <React.Fragment key={key}>
+            <QuickReplyElement data={quickReply} />
+          </React.Fragment>
+        )
+      })}
+    </Flex>
+  )
 }
 
 QuickRepliesElement.propTypes = {
