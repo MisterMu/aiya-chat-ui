7eba24c0de13b09d7fa26a46313b05db17aa5b49
Pakin Suwannawat <painter.suwannawat@gmail.com>
2018-10-16 20:43:00 +0700

    check message.quick_replies before update

--- src/lib/MessageForm/Facebook/QuickReplies/index.js
+++ src/lib/MessageForm/Facebook/QuickReplies/index.js
@@ -7,14 +7,16 @@ import QuickReplyObject from '../../../MessageObject/Facebook/quickReply'
 class QuickRepliesForm extends BaseMessageForm {
   inputChange = (item, index) => {
     const { message } = this.state
-    let tmp = [...message.quick_replies]
-    const oldData = { ...tmp[index] }
-    if (item.content_type && item.content_type !== oldData.content_type) {
-      tmp[index] = QuickReplyObject[item.content_type]
-    } else {
-      tmp[index] = { ...oldData, ...item }
+    if (message.quick_replies) {
+      let tmp = [...message.quick_replies]
+      const oldData = { ...tmp[index] }
+      if (item.content_type && item.content_type !== oldData.content_type) {
+        tmp[index] = QuickReplyObject[item.content_type]
+      } else {
+        tmp[index] = { ...oldData, ...item }
+      }
+      this.setState({ message: { ...message, quick_replies: tmp } })
     }
-    this.setState({ message: { ...message, quick_replies: tmp } })
   }
 
   renderForm = () => {
