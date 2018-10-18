681dbe9b67cbc129b201d144b131ad8a893c3b4c
Pakin Suwannawat <painter.suwannawat@gmail.com>
2018-10-18 15:52:04 +0700

    add validate to Line QuickReply MessageForm

--- src/lib/MessageForm/Line/QuickReplies/index.js
+++ src/lib/MessageForm/Line/QuickReplies/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Divider, Icon } from 'antd'
 import QuickReplyForm from './QuickReply'
 import BaseMessageForm from '../../BaseMessageForm'
-import QuickReplyObject from '../../../MessageObject/Line/quickReply'
+import QuickReplyObject, { Types } from '../../../MessageObject/Line/quickReply'
 import { swapArrayElement } from '../../../../utils'
 import { Flex, Toolbar } from '../../styled'
 
@@ -72,6 +72,80 @@ class QuickRepliesForm extends BaseMessageForm {
     this.setState({ message: { ...message, quickReply: { items: tmp } } })
   }
 
+  validateMessage = () => {
+    const { message } = this.state
+    const quickReplies = message.quickReply && [...message.quickReply.items]
+    return quickReplies.every((quickReply, i) => {
+      if (quickReply.imageUrl && quickReply.imageUrl.substr(0, 8) !== 'https://') {
+        this.setState({ error: `Quick Reply #${i + 1}: Image url schema must be a https!!` })
+        return false
+      }
+      if (!quickReply.action.label) {
+        this.setState({ error: `Quick Reply #${i + 1}: Label is required!!` })
+        return false
+      }
+      if (quickReply.action.label && quickReply.action.label.length > 20) {
+        this.setState({
+          error: `Quick Reply #${i + 1}: Label must be no longer than 20 characters!!`,
+        })
+        return false
+      }
+      if (quickReply.action.type === Types.POSTBACK) {
+        if (!quickReply.action.data) {
+          this.setState({ error: `Quick Reply #${i + 1}: Postback data is required!!` })
+          return false
+        }
+        if (quickReply.action.data.length > 300) {
+          this.setState({ error: `Quick Reply #${i + 1}: Postback data must be no longer than 300 characters!!` })
+          return false
+        }
+        if (!quickReply.action.displayText) {
+          this.setState({ error: `Quick Reply #${i + 1}: Postback displayText is required!!` })
+          return false
+        }
+        if (quickReply.action.displayText.length > 300) {
+          this.setState({
+            error: `Quick Reply #${i + 1}: Postback displayText must be no longer than 300 characters!!`,
+          })
+          return false
+        }
+      } else if (quickReply.action.type === Types.MESSAGE) {
+        if (!quickReply.action.text) {
+          this.setState({ error: `Quick Reply #${i + 1}: Message text is required!!` })
+          return false
+        }
+        if (quickReply.action.text.length > 300) {
+          this.setState({ error: `Quick Reply #${i + 1}: Message text must be no longer than 300 characters!!` })
+          return false
+        }
+      } else if (quickReply.action.type === Types.URI) {
+        const regEx = /(^https:\/\/|^http:\/\/|^line:\/\/|^tel:\/\/)/g
+        if (!quickReply.action.uri) {
+          this.setState({ error: `Quick Reply #${i + 1}: URI link is required!!` })
+          return false
+        }
+        if (!regEx.test(quickReply.action.uri)) {
+          this.setState({ error: `Quick Reply #${i + 1}: URI link schema must be one of http, https, line, tel!!` })
+          return false
+        }
+        if (quickReply.action.uri.length > 1000) {
+          this.setState({ error: `Quick Reply #${i + 1}: URI link must be no longer than 1000 characters!!` })
+          return false
+        }
+      } else if (quickReply.action.type === Types.DATE) {
+        if (!quickReply.action.data) {
+          this.setState({ error: `Quick Reply #${i + 1}: DatePicker data is required!!` })
+          return false
+        }
+        if (quickReply.action.data.length > 300) {
+          this.setState({ error: `Quick Reply #${i + 1}: DatePicker data must be no longer than 300 characters!!` })
+          return false
+        }
+      }
+      return true
+    })
+  }
+
   renderForm = () => {
     const { message } = this.state
     if (!message || !message.quickReply) {
@@ -85,18 +159,10 @@ class QuickRepliesForm extends BaseMessageForm {
               <Divider orientation="left">Quick Reply #{i + 1}</Divider>
               <Toolbar>
                 {i !== 0 && (
-                  <Icon
-                    type="up-circle"
-                    className="primary-icon"
-                    onClick={() => this.moveQuickReply(i, 'up')}
-                  />
+                  <Icon type="up-circle" className="primary-icon" onClick={() => this.moveQuickReply(i, 'up')} />
                 )}
                 {i !== message.quickReply.items.length - 1 && (
-                  <Icon
-                    type="down-circle"
-                    className="primary-icon"
-                    onClick={() => this.moveQuickReply(i, 'down')}
-                  />
+                  <Icon type="down-circle" className="primary-icon" onClick={() => this.moveQuickReply(i, 'down')} />
                 )}
                 {message.quickReply.items.length !== 1 && (
                   <Icon
@@ -112,8 +178,8 @@ class QuickRepliesForm extends BaseMessageForm {
           </React.Fragment>
         ))}
         <Divider />
-        {message.quickReply.items.length < 10 && (
-          <Flex style={{ cursor: 'pointer' }} onClick={this.addQuickReply}>
+        {message.quickReply.items.length < 13 && (
+          <Flex style={{ cursor: 'pointer', justifyContent: 'center' }} onClick={this.addQuickReply}>
             <Icon type="plus-circle" />
             <span style={{ marginLeft: 8 }}>Add QuickReply</span>
           </Flex>
