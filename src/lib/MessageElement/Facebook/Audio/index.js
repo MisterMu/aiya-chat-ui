6bcf2cdc453a5055ddba395c7f4d8a4e4472e943
Pakin Suwannawat <painter.suwannawat@gmail.com>
2018-10-19 17:06:18 +0700

    remove BubbleMessage from audio MessageElement

--- src/lib/MessageElement/Facebook/Audio/index.js
+++ src/lib/MessageElement/Facebook/Audio/index.js
@@ -1,18 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { BubbleMessage } from '../../styled'
-import { color } from '../../../../constants'
-
-const { FACEBOOK } = color
 
 const AudioElement = props => {
   const { url, onClick } = props
   return (
-    <BubbleMessage color={FACEBOOK.bubbleColor} textColor={FACEBOOK.textColor} onClick={onClick}>
-      <audio controls>
-        <source src={url} />
-      </audio>
-    </BubbleMessage>
+    <audio controls onClick={onClick}>
+      <source src={url} />
+    </audio>
   )
 }
 
--- src/lib/MessageElement/Line/Audio/index.js
+++ src/lib/MessageElement/Line/Audio/index.js
@@ -1,18 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { BubbleMessage } from '../../styled'
-import { color } from '../../../../constants'
-
-const { LINE } = color
 
 const AudioElement = props => {
   const { url, onClick } = props
   return (
-    <BubbleMessage color={LINE.bubbleColor} textColor={LINE.textColor} onClick={onClick}>
-      <audio controls>
-        <source src={url} />
-      </audio>
-    </BubbleMessage>
+    <audio controls onClick={onClick}>
+      <source src={url} />
+    </audio>
   )
 }
 
