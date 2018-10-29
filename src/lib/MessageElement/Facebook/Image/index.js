d5196e6114b813ade06c5edf2eb2a4c8d14fa0a9
Pakin Suwannawat <painter.suwannawat@gmail.com>
2018-10-29 18:31:38 +0700

    fix preview image in image MessageElement and imagemap MessageElement

--- src/lib/MessageElement/Facebook/Image/index.js
+++ src/lib/MessageElement/Facebook/Image/index.js
@@ -7,7 +7,7 @@ const ImageElement = props => {
   const { url, onClick } = props
   return (
     <ImageContainer onClick={onClick}>
-      <img src={url || defaultImage} width={320} height={320} alt="ImageMessage" />
+      <img src={url || defaultImage} width={320} alt="ImageMessage" />
     </ImageContainer>
   )
 }
